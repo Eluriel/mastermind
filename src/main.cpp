1f48c3cf7419a25367498faa2bcde1d212c56225
Eluriel <eluriel9@gmail.com>
2022-04-09 22:13:45 +0200

    Display LED when submitting result

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ byte rowPins[4] = {rowPin1, rowPin2, rowPin3, rowPin4};
 byte colPins[4] = {colPin1, colPin2, colPin3, colPin4};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-Adafruit_NeoPixel pixels(4, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(4, LED_PIN, NEO_RGB);
 
 Digit digits[] = {
     Digit(0, 0, digitPin1),
@@ -31,7 +31,8 @@ void setup()
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   pixels.clear(); // Set all pixel colors to 'off'
-  pixels.show();  // Send the updated pixel colors to the hardware.
+  pixels.setBrightness(64);
+  pixels.show(); // Send the updated pixel colors to the hardware.
 
   // Choose solution
   Digit::InitSolution();
@@ -47,24 +48,64 @@ void loop()
     switch (key)
     {
     case '*':
-      Digit::ClearAllDigits();
+      ClearData();
       break;
     case '#':
-      Digit::SubmitAnswer();
+      SendAnswer();
       break;
     default:
-      Digit *selectedDigit = Digit::GetSelectedDigit();
-      if (selectedDigit == NULL)
-      {
-        // No digit selected. Select first digit, clear and set value
-        Digit::SetSelectedDigitToFirst();
-        Digit::ClearAllDigits();
-        selectedDigit = Digit::GetSelectedDigit();
-      }
-      selectedDigit->SetValue(key - '0');
-      selectedDigit->SetSelectedDigitToNext();
+      WriteKey(key);
       break;
     }
   }
   Digit::Update();
+}
+
+void ClearData()
+{
+  Digit::ClearAllDigits();
+  pixels.clear();
+  pixels.show();
+}
+
+void WriteKey(char key)
+{
+  Digit *selectedDigit = Digit::GetSelectedDigit();
+  if (selectedDigit == NULL)
+  {
+    // No digit selected. Select first digit, clear and set value
+    Digit::SetSelectedDigitToFirst();
+    ClearData();
+    selectedDigit = Digit::GetSelectedDigit();
+  }
+  selectedDigit->SetValue(key - '0');
+  selectedDigit->SetSelectedDigitToNext();
+}
+
+void SendAnswer()
+{
+  std::pair<int, int> reply = Digit::SubmitAnswer();
+  int nbFull = reply.first;
+  int nbHalf = reply.second;
+  std::cout << "Results: " << nbFull << " full, " << nbHalf << " half." << std::endl;
+
+  // Display results on LEDs
+  pixels.clear();
+
+  for (int i = 0; i < 4; i++)
+  {
+    if (i < nbFull)
+    {
+      pixels.setPixelColor(i, 0, 204, 0);
+    }
+    else if (i < (nbFull + nbHalf))
+    {
+      pixels.setPixelColor(i, 0, 0, 204);
+    }
+    else
+    {
+      pixels.setPixelColor(i, 204, 0, 0);
+    }
+  }
+  pixels.show();
 }
